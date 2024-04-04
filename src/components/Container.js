e23e6630553f89384da8def063e84a33925b56ab
Le Thi Thu Nhung <lethunhung94@gmail.com>
2024-04-05 00:54:50 +0700

    them tep container.js

---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Container = (props) => {
+    return (
+        <section className={props.class1}>
+            <div className="conatainer-xxl">(props.children)</div>
+        </section>
+    );
+};
+export default Container;
